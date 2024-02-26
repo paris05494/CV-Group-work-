eb99902f34f7c1bf1ff06a0d832861912b28c224
inwchamp1337 <ppasinb@gmail.com>
2024-02-26 16:21:52 +0700

    sus1

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Connect to server and select database.
-define('servername', 'localhost');
-define('username', 'root');
-define('password', '');
-define('dbname', 'myhotel');
+define('servername', 'sql203.infinityfree.com');
+define('username', 'if0_36051526');
+define('password', '0831809789Aa');
+define('dbname', 'if0_36051526_test');
 
 $objCon = mysqli_connect(servername, username, password,dbname);
 ?>
